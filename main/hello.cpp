5349ee58edf1902813c80bb23601fdf948281bb2
KarthikHK-01 <hkkarthik012@gmail.com>
2025-03-15 15:18:05 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n, I am PES1UG22CS277";
+  cout << "Hello, World\n, I am PES1UG22CS277, Karthik H Kademani\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
