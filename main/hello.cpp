c6f22cd941a129b121fdd4130ad5123ca5f59989
Karthik H Kademani <hkkarthik012@gmail.com>
2025-03-17 13:00:48 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n, I am PES1UG22CS277, Karthik H Kademani\n";
   cout << "Hello, Jenkins\n ";
-  cout << "I - PES1UG22CS277 , have successfully built and run this Jenkins Lab...  \n";
+  cout << "I - PES1UG22CS277 , have successfully built and run this Jenkins Lab.  \n";
   return 0;
 }
